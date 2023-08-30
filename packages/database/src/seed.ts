a2eb55403f4087aa5d9f003f8ff9c220b366a974
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2023-08-30 12:05:32 +0900

    improve base structure

---
+++ packages/database/src/seed.ts
@@ -0,0 +1,34 @@
+import { prisma } from ".";
+
+const DEFAULT_USERS = [
+  // Add your own user to pre-populate the database with
+  {
+    name: "Perfect Base",
+    email: "perfect@base.com",
+  },
+];
+
+(async () => {
+  try {
+    await Promise.all(
+      DEFAULT_USERS.map((user) =>
+        prisma.user.upsert({
+          where: {
+            email: user.email!,
+          },
+          update: {
+            ...user,
+          },
+          create: {
+            ...user,
+          },
+        })
+      )
+    );
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+})();
